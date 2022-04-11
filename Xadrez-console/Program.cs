779a90cea4b15b84871ed88b6697918b07e09ddb
Juliana Andrade <juliana.andraden07@gmail.com>
2022-04-11 15:44:46 -0300

    Iniciando o tabuleiro

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -6,6 +6,9 @@ namespace Xadrez_console
     class Program
     {
         static void Main(string[] args) {
+
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.ReadLine();
             
         }
     }
--- Xadrez-console/tabuleiro/Posicao.cs
+++ Xadrez-console/tabuleiro/Posicao.cs
@@ -5,6 +5,9 @@
         public int linha { get; set; }
         public int coluna { get; set; }
 
+        public Posicao()
+        {
+        }
         public Posicao(int linha, int coluna)
         {
             this.linha = linha;
