a2c20e690c737aecfb479c0c4804b81a75590014
Juliana Andrade <juliana.andraden07@gmail.com>
2022-04-11 17:12:10 -0300

    Erros com excecoes personalizadas

---
+++ Xadrez-console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException()
+        {
+        }
+        public TabuleiroException(string msg) : base(msg)
+        {
+        }
+    }
+}
